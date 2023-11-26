087dfb82d391fdf425bceccd4006d662a053ab2f
wjf0214 <a846930164@gmail.com>
2023-11-26 15:33:20 +0800

    feat(IT天空): 每日签到

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ql-script
+
 基于qinglong任务管理平台进行开发学习测试的脚本库。
+
+[![作者](https://img.shields.io/badge/Author-wjf0214-blueviolet)](https://github.com/wjf0214/ )
+![关注数量](https://img.shields.io/badge/dynamic/json?label=GitHub%20Followers&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dgithub%26queryKey%3Dwjf0214&labelColor=282c34&color=181717&logo=github&longCache=true)
+![Star数量](https://img.shields.io/github/stars/wjf0214/ql-script.svg?style=flat&&logo=appveyor)
+![Fork数量](https://img.shields.io/github/forks/wjf0214/ql-script.svg?style=flat&&logo=stackshare)
+![贡献者](https://img.shields.io/github/contributors/wjf0214/ql-script)
+![文件大小](https://img.shields.io/github/repo-size/wjf0214/ql-script?style=flat&label=files&color=cf8ef4&labelColor=373e4dl)
+![最后提交时间](https://img.shields.io/github/last-commit/wjf0214/ql-script.svg?style=float)
+
+## 🏠简介
+
+> 项目基于开源的 [青龙](https://github.com/whyour/qinglong) 定时任务管理平台使用，仅供学习和交流使用，不能保证其合法性、准确性、完整性和有效性。
+>
+> 每个脚本的使用方法详见脚本注释
+
+## 📃免责声明
+
+本仓库发布的脚本及其中涉及的任何解密分析脚本，仅供个人学习与交流使用，严禁用于商业、侵权、侵害他人利益、违法违禁等不良用途。并且由此造成的一切后果，本人概不负责。且本项目内所有资源文件，禁止任何公众号、自媒体进行任何形式的转载、发布。您必须在下载后的24小时内从计算机或手机中完全删除以上内容。请悉知！
+
+## 👤游客访问
+
+<p align="center">
+   <img alingn="center" src="https://profile-counter.glitch.me/wjf0214-ql-script/count.svg"  alt="ql-script"/>
+ </p>
+
+## 🖥️Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=wjf0214/ql-script&type=Date)](https://star-history.com/#wjf0214/ql-script&Date)
---
+++ itsk.py
@@ -0,0 +1,130 @@
+# !/usr/bin/python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : wjf0214
+# @Time : 2023/11/25 23:23
+# -------------------------------
+# cron "30 8 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('IT天空网站签到')
+
+"""
+IT天空网站签到
+export itskCookie = 'access_itsk=Bearer***',多账号使用换行或&
+cookie取网站首页cookie
+"""
+
+import requests
+import os
+import sys
+import time
+import random
+import string
+import hashlib
+
+from urllib.parse import unquote
+
+ck_list = []
+
+sign_url = "https://api.itsk.com/webapi/sign"
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'accept-encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Referer': 'https://www.itsk.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    'Sec-Ch-Ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    'Sec-Ch-Ua-Mobile': "?0",
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Origin': 'https://www.itsk.com'
+}
+
+
+# 签到
+def sign_in(cookie):
+    try:
+        access_itsk = get_cookie_value(cookie, 'access_itsk')
+        headers = get_header(access_itsk)
+
+        # {"code":200,"message":"签到成功","data":{"total":1,"credit":"活跃","message":null,"month_days":2,"days":697,"status":0}}
+        response = requests.get(sign_url, headers=headers).json()
+
+        if response.get('code') == 200:
+            if response.get('data').get('status') == 0:
+                msg = f"{response.get('message')} --- 恭喜获得 {response.get('data').get('total')} {response.get('data').get('credit')} , 本月签到共 {response.get('data').get('month_days')} 天，总共签到 {response.get('data').get('days')} 天"
+            else:
+                msg = f"{response.get('message')} --- 累计获得 {response.get('data').get('total')} {response.get('data').get('credit')} , 本月签到共 {response.get('data').get('month_days')} 天，总共签到 {response.get('data').get('days')} 天"
+            print(msg)
+        else:
+            print(f"签到失败！--- {response.get('message')}")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+
+
+# 获取环境变量
+def get_env():
+    global ck_list
+    env_str = os.getenv("itskCookie")
+    if env_str:
+        ck_list += env_str.replace("&", "\n").split("\n")
+
+
+# 随机字符串
+def nonce(t=32):
+    e = string.ascii_letters + string.digits
+    r = ''.join(random.choice(e) for _ in range(t))
+    return r
+
+
+# sha1加密
+def sha1(t, e):
+    n = e[:4]
+    r = e[-4:]
+    return hashlib.sha1(f"{n}{t}{r}{e}".encode('utf-8')).hexdigest()
+
+
+# 获取请求头
+def get_header(access_itsk):
+    global headers
+    timestamp = int(time.time())
+    nonceStr = nonce()
+    encrypt = sha1(timestamp, nonceStr)
+    header = {
+        "Authorization": access_itsk,
+        "X-Request-timestamp": str(timestamp),
+        "X-Request-signature": encrypt,
+        "X-Request-nonce": nonceStr
+    }
+    return {**header, **headers}
+
+
+# 获取cookie
+def get_cookie_value(cookie, key):
+    cookie = unquote(cookie)
+    cookie_parts = cookie.split(";")
+    for part in cookie_parts:
+        if key in part:
+            return part.split("=")[1]
+    return None
+
+
+def main():
+    get_env()
+
+    if not ck_list:
+        print('没有获取到cookie。')
+        return
+
+    print(f'获取到{len(ck_list)}个账号！')
+
+    for index, ck in enumerate(ck_list):
+        print(f'*****第{index + 1}个账号*****')
+        # 签到
+        sign_in(ck)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
